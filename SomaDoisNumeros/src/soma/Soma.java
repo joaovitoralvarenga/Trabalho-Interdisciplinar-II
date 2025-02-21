e7fc4694d6dbda77e516892af8722da153be9064
joaovitoralvarenga <ggquennbr@gmail.com>
2025-02-20 21:51:36 -0300

    Inserção da pasta destinada ao exercicio 1

---
+++ SomaDoisNumeros/src/soma/Soma.java
@@ -0,0 +1,27 @@
+package soma;
+import java.util.*;
+
+	 class SomaDoisNumeros {
+		 public static Scanner sc = new Scanner(System.in);
+		 
+		 
+	     public static void main(String[] args) {
+	         Scanner sc = new Scanner(System.in); // Criando Scanner dentro do método main
+
+	         System.out.println("Digite um número:");
+	         int num1 = sc.nextInt();
+	         
+	         System.out.println("Digite outro número:");
+	         int num2 = sc.nextInt();
+	         
+	         int soma = num1 + num2;
+	         
+	         System.out.println("Soma: " + soma); // Adicionando um espaço para melhorar a formatação
+	         
+	       
+	     }
+	 }
+	
+	
+
+

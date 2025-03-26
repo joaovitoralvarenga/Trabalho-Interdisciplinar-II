ae093e3b7e37eb5d0a494b711f802d25c333c7e1
joaovitoralvarenga <ggquennbr@gmail.com>
2025-03-26 09:18:45 -0300

    Criação da pasta do projeto e inserção do primeiro código

---
+++ Integraçaõ Spark - CRUD de Produtos/Project/crud-spark/src/test/java/com/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
